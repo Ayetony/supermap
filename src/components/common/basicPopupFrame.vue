666bcb535653c3bb8ee8ef8c260d4609664df025
miao.ayetony <miao.ayetony@gmail.com>
2022-02-22 10:45:00 +0800

    Update basicPopupFrame.vue
    for compatible issue to update

--- src/components/common/basicPopupFrame.vue
+++ src/components/common/basicPopupFrame.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <h2 style="color: #0868e5;float: right;height: 5px;">
+    <h2 style="color: #0868e5;float: right;height: 5px;margin-top:-10vh;position: relative">
       <button @click="closePopup">关闭</button>
     </h2>
-    <h5 style="color: #0868e5;margin-top: -60px;padding: 5px;">详细信息</h5>
+    <h5 style="color: #0868e5;line-height: 0px;padding-bottom: 10vh;">详细信息</h5>
   </div>
 </template>
 <script>
